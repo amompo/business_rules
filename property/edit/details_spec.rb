4c5ae4781a0787709fa998fb3bd58cafb595d4fd
Alex Mompo Mikkelsen <alex_mompo@yahoo.com>
2013-03-17 14:07:48 +0100

    edit

---
+++ property/edit/details_spec.rb
@@ -0,0 +1,25 @@
+describe 'Property' do
+	describe 'Edit' do
+		describe 'Details' do
+			describe 'Type' do
+				describe 'Change to room' do
+					it 'changes type to room'
+
+					end
+
+					 it 'updates number of rooms to one' do
+					 end
+				end
+
+				describe 'Change to apartment' do
+					it 'changes type to apartment'
+
+					end
+
+					 it 'updates number of rooms to one' do
+					 end
+				end
+			end
+		end
+	end
+end
